3b0e785c55baa0cd8e9a7ccedd3d5847d351d138
cbond <cbond@44f0b718-a8ad-45a4-bab0-870672037115>
2002-01-28 06:23:41 +0000

    Add the standard header message.
    git-svn-id: https://svn.code.sf.net/p/lurker/code/trunk@38 44f0b718-a8ad-45a4-bab0-870672037115

--- lurker/common/main.c
+++ lurker/common/main.c
@@ -1,6 +1,26 @@
 /*
- * Copyright 2002 Chris L. Bond.  All rights reserved.
- * $Id: main.c,v 1.3 2002-01-28 06:15:47 terpstra Exp $
+ * $Id: main.c,v 1.4 2002-01-28 06:23:41 cbond Exp $
+ *  
+ *  main.c - debugging test for message parsing routines.
+ *  
+ *  Copyright 2002 Chris L. Bond.  All rights reserved.
+ *  
+ *  License: GPL
+ *  
+ *  Authors: 'Chris L. Bond' <cbond@twistedcircus.org>
+ *  
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; version 2.1.
+ *    
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *    
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
 #include <stdio.h>
--- lurker/common/stub.c
+++ lurker/common/stub.c
@@ -1,6 +1,27 @@
 /*
- * Copyright 2002 Chris L. Bond.  All rights reserved.
- * $Id: stub.c,v 1.2 2002-01-28 06:15:47 terpstra Exp $
+ * $Id: stub.c,v 1.3 2002-01-28 06:23:41 cbond Exp $
+ *  
+ *  stub.c - stub out c-client logging interfaces.
+ *  
+ *  Copyright 2002 Chris L. Bond.  All rights reserved.
+ *  
+ *  License: GPL
+ *  
+ *  Authors: 'Chris L. Bond' <cbond@twistedcircus.org>
+ *  
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; version 2.1.
+ *    
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *    
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
  */
 
 #include <sys/param.h>
