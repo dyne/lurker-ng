3beaedd0e8fbdc2eaea8be18c8cf7305bd1b5d4c
terpstra <terpstra@44f0b718-a8ad-45a4-bab0-870672037115>
2002-02-04 00:14:31 +0000

    The protocol for talking with the cgi
    git-svn-id: https://svn.code.sf.net/p/lurker/code/trunk@71 44f0b718-a8ad-45a4-bab0-870672037115

---
+++ lurker/common/protocol.h
@@ -0,0 +1,46 @@
+/*  $Id: protocol.h,v 1.1 2002-02-04 00:14:31 terpstra Exp $
+ *  
+ *  protocol.h - What do we use to communicate
+ *  
+ *  Copyright (C) 2002 - Wesley W. Terpstra
+ *  
+ *  License: GPL
+ *  
+ *  Authors: 'Wesley W. Terpstra' <wesley@terpstra.ca>
+ *  
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; version 2.1.
+ *    
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *    
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#define LU_PROTO_GETMSG		"getmsg "
+
+typedef struct Lu_Proto_Message_T
+{
+	lu_word		list;
+	lu_word		mbox;
+	
+	char 		list_name[80];
+	char		list_address[80];
+	char		list_desc[200];
+	char		mbox_name[100];
+	
+	lu_quad		timestamp;
+	message_id	in_reply_to;
+	message_id	thread_parent;
+	
+	lu_word		replys;		/* How many replys are there */
+	lu_quad		length;		/* The length of the message */
+	
+	message_id	reply[0];	/* All the replys to this message */
+	char		body[0];	/* The message follows */
+} Lu_Proto_Message;
