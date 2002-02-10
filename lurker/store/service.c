d34d77eb5f91f6292604487620f59af7b4b58491
terpstra <terpstra@44f0b718-a8ad-45a4-bab0-870672037115>
2002-02-10 21:47:12 +0000

    Eek! Wrong method
    git-svn-id: https://svn.code.sf.net/p/lurker/code/trunk@113 44f0b718-a8ad-45a4-bab0-870672037115

--- lurker/store/service.c
+++ lurker/store/service.c
@@ -1,4 +1,4 @@
-/*  $Id: service.c,v 1.10 2002-02-10 21:07:38 terpstra Exp $
+/*  $Id: service.c,v 1.11 2002-02-10 21:47:12 terpstra Exp $
  *  
  *  service.c - Knows how to deal with request from the cgi
  *  
@@ -514,7 +514,7 @@ static int my_service_mindex(st_netfd_t fd, const char* request)
 			"Lurkerd received a mindex request past the end of the list",
 			request);
 		
-		lu_breader_close(h);
+		lu_breader_free(h);
 		return -1;
 	}
 	
@@ -529,7 +529,7 @@ static int my_service_mindex(st_netfd_t fd, const char* request)
 			"Lurkerd failed to retrieve the records from the keyword file",
 			"server failure - see log files");
 		
-		lu_breader_close(h);
+		lu_breader_free(h);
 		return -1;
 	}
 	
@@ -554,7 +554,7 @@ static int my_service_mindex(st_netfd_t fd, const char* request)
 	}
 	
 	my_service_list(fd, l, lu_breader_records(h));
-	lu_breader_close(h);
+	lu_breader_free(h);
 	
 	for (i = 0; i < count; i++)
 	{
