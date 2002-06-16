3dd5bf787749ebf977e1f8808e0da47ab4461a6a
terpstra <terpstra@44f0b718-a8ad-45a4-bab0-870672037115>
2002-06-16 08:44:54 +0000

    Much better ASCII art detection. Also, don't count : as quote, I get too many false positives.
    git-svn-id: https://svn.code.sf.net/p/lurker/code/trunk@380 44f0b718-a8ad-45a4-bab0-870672037115

--- reg2c/host.c
+++ reg2c/host.c
@@ -8,7 +8,7 @@
 #define PATH_REG	"((/" FILE_REG ")+)"
 #define POST_REG   	"([A-Za-z%~0-9.,_=/:;+\\&\\-]+)"
 #define PROTO_REG   	"([a-zA-Z]+)"
-#define INDENT_REG 	"([a-zA-Z]?[a-zA-Z]?[a-zA-Z]?[a-zA-Z]?[>:] )"
+#define INDENT_REG 	"([a-zA-Z]?[a-zA-Z]?[a-zA-Z]?[a-zA-Z]?>)"
 
 #define URL_REG    	"(((" PROTO_REG "://" HOST_REG "(" PATH_REG ")?)" \
                          "|(www\\." HOST_REG "\\." TOKEN_REG ")" \
@@ -23,9 +23,14 @@
 #define NOT_LF		"[\001-\011\013-\377]"
 #define QUOTE_REG       "(\n" INDENT_REG NOT_LF "*\n(" INDENT_REG NOT_LF "*\n)*)"
 
-#define ASCII_ART_REG   "((" NOT_LF "*  " NOT_LF "*  " NOT_LF "*)" \
-			"|(" NOT_LF "*\t" NOT_LF "*))"
-#define ASCII_PIC_REG   "(\n" ASCII_ART_REG "\n(" ASCII_ART_REG "\n)*)"
+#define ASCII_ART_REG   "(" NOT_LF "*" \
+			"[\001-\011\013-\037\041-\\\055\057-\075\077-\377]  " NOT_LF "*" \
+			"[\001-\011\013-\037\041-\\\055\057-\075\077-\377]  " NOT_LF "*\n)"
+#define ASCII_ROWS_REG	"(" NOT_LF "+\n)*"
+
+#define ASCII_PIC_REG   "\n" ASCII_ROWS_REG ASCII_ART_REG \
+			     ASCII_ROWS_REG ASCII_ART_REG \
+			     ASCII_ROWS_REG
 
 int main(int argc, const char** argv)
 {
