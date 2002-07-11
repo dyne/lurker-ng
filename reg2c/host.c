1d37151e784fcf721fe231233c9be4389f5ffdf7
terpstra <terpstra@44f0b718-a8ad-45a4-bab0-870672037115>
2002-07-11 22:28:55 +0000

    Support a hostname scan Files cannot end in punctutation -- this way we avoid marking urls with blah.blah.txt. with the ending . (probably a period for the sentence) Redundant PATH directive removed # is a special regexp char, escape it
    git-svn-id: https://svn.code.sf.net/p/lurker/code/trunk@536 44f0b718-a8ad-45a4-bab0-870672037115

--- reg2c/host.c
+++ reg2c/host.c
@@ -4,17 +4,17 @@
 #define TOKEN_REG       "([A-Za-z0-9][a-zA-Z0-9\\-]*[a-zA-Z0-9]|[a-zA-Z0-9])"
 #define HOST_REG	"((" TOKEN_REG "\\.)*" TOKEN_REG ")"
 #define USER_REG	"([A-Za-z0-9][a-zA-Z0-9._=\\-]*[a-zA-Z0-9]|[a-zA-Z0-9])"
-#define FILE_REG        "([A-Za-z%~0-9:.,_\\-]+)"
+#define FILE_REG        "([A-Za-z%~0-9:.,_\\-]*[A-Za-z0-9])"
 #define PATH_REG	"((/" FILE_REG ")+)"
 #define POST_REG   	"([A-Za-z%~0-9.,_=/:;+\\&\\-]+)"
 #define PROTO_REG   	"([a-zA-Z]+)"
 #define INDENT_REG 	"([a-zA-Z]?[a-zA-Z]?[a-zA-Z]?[a-zA-Z]?>)"
 
-#define URL_REG    	"(((" PROTO_REG "://" HOST_REG "(" PATH_REG ")?)" \
+#define URL_REG    	"(((" PROTO_REG "://" HOST_REG ")" \
                          "|(www\\." HOST_REG "\\." TOKEN_REG ")" \
                          ")" \
                          "("    PATH_REG ")?" \
-                    	 "(#"   FILE_REG ")?" \
+                    	 "(\\#" FILE_REG ")?" \
                     	 "(\\?" POST_REG ")?" \
                         ")"
 
@@ -34,6 +34,7 @@
 
 int main(int argc, const char** argv)
 {
+	if (!strcmp(argv[1], "host"))	write(1, HOST_REG,      sizeof(HOST_REG)-1);
 	if (!strcmp(argv[1], "url"))	write(1, URL_REG,       sizeof(URL_REG)-1);
 	if (!strcmp(argv[1], "mailto"))	write(1, EMAIL_REG,     sizeof(EMAIL_REG)-1);
 	if (!strcmp(argv[1], "quote"))	write(1, QUOTE_REG,     sizeof(QUOTE_REG)-1);
