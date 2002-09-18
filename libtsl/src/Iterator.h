cb5de38efdd40c71a0fcf9426cde520fb2587f15
terpstra <terpstra@44f0b718-a8ad-45a4-bab0-870672037115>
2002-09-18 18:53:27 +0000

    This commit was generated by cvs2svn to compensate for changes in r729, which included commits to RCS files with non-trunk default branches.
    git-svn-id: https://svn.code.sf.net/p/lurker/code/trunk@730 44f0b718-a8ad-45a4-bab0-870672037115

---
+++ libtsl/src/Iterator.h
@@ -0,0 +1,24 @@
+/*  $Id: Iterator.h,v 1.1.1.1 2002-09-18 18:53:27 terpstra Exp $
+ *  
+ *  expiry.c - Record when pages should be destroyed
+ *  
+ *  Copyright (C) 2002 - Wesley W. Terpstra
+ *  
+ *  License: GPL
+ *  
+ *  Authors: 'Wesley W. Terpstra' <wesley@terpstra.ca>
+ *  
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; version 2.1.
+ *    
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *    
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
