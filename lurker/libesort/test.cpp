cc91e03b2d357daf390406576c66895a35e24311
terpstra <terpstra@44f0b718-a8ad-45a4-bab0-870672037115>
2003-04-25 11:06:35 +0000

    time to debug
    git-svn-id: https://svn.code.sf.net/p/lurker/code/trunk@944 44f0b718-a8ad-45a4-bab0-870672037115

---
+++ lurker/libesort/test.cpp
@@ -0,0 +1,207 @@
+/*  $Id: test.cpp,v 1.1 2003-04-25 11:06:35 terpstra Exp $
+ *  
+ *  test.cpp - Write lots of crap and compare with RAM
+ *  
+ *  Copyright (C) 2002 - Wesley W. Terpstra
+ *  
+ *  License: GPL
+ *  
+ *  Authors: 'Wesley W. Terpstra' <wesley@terpstra.ca>
+ *  
+ *    This program is free software; you can redistribute it and/or modify
+ *    it under the terms of the GNU General Public License as published by
+ *    the Free Software Foundation; version 2.1.
+ *    
+ *    This program is distributed in the hope that it will be useful,
+ *    but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *    GNU General Public License for more details.
+ *    
+ *    You should have received a copy of the GNU General Public License
+ *    along with this program; if not, write to the Free Software
+ *    Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#define _XOPEN_SOURCE 500
+#define _BSD_SOURCE
+#define _ISOC99_SOURCE
+  
+#include "esort.h"
+
+#include <iostream>
+#include <set>
+
+#include <cstdio>
+#include <cstdlib>
+#include <cerrno>
+#include <cassert>
+
+using namespace std;
+using namespace ESort;
+
+multiset<string> ram;
+auto_ptr<Writer> db;
+
+string makeStr()
+{	// for testing with a human
+	int len = random() % 20;
+	int i;
+	
+	string out;
+	for (i = 0; i < len; ++i)
+		out += char('A' + (random() % ('Z' - 'A' + 1)));
+	
+	return out;
+}
+
+string makeBin()
+{	// for testing without a human
+	int len = random() % 256;
+	int i;
+	
+	string out;
+	for (i = 0; i < len; ++i)
+		out += char(random() % 256);
+	
+	return out;
+}
+
+void check_forward(const string& k)
+{
+	multiset<string>::iterator i;
+	int eo;
+	
+	auto_ptr<Walker> walk(db->seek(k, true));
+	assert (walk.get()); // always succeeds
+	
+	for (eo = walk->advance(), i = ram.lower_bound(k);
+	     eo != -1 && i != ram.end();
+	     eo = walk->advance(), ++i)
+	{
+		if (walk->key != *i)
+		{
+			cerr << "Wrong forward keys: " << walk->key << " != " << *i << endl;
+			exit(1);
+		}
+	}
+	
+	if (eo == -1 && i != ram.end())
+	{
+		cerr << "Ram has: " << *i << endl;
+		perror("db forward underrun vs ram");
+		exit(1);
+	}
+	
+	if (eo == 0 && i == ram.end())
+	{
+		cerr << "Db has: " << walk->key << endl;
+		cerr << "db forward overrun vs ram" << endl;
+		exit(1);
+	}
+}
+
+void check_backward(const string& k)
+{
+	multiset<string>::iterator i;
+	int eo;
+	
+	auto_ptr<Walker> walk(db->seek(k, false));
+	assert (walk.get()); // always succeeds
+	
+	for (eo = walk->advance(), i = ram.lower_bound(k);
+	     eo != -1 && i != ram.begin();
+	     eo = walk->advance())
+	{
+		--i;
+		
+		if (walk->key != *i)
+		{
+			cerr << "Wrong backward keys: " << walk->key << " != " << *i << endl;
+			exit(1);
+		}
+	}
+	
+	if (eo == -1 && i != ram.begin())
+	{
+		cerr << "Ram has: " << *--i << endl;
+		perror("db backward underrun vs ram");
+		exit(1);
+	}
+	
+	if (eo == 0 && i == ram.end())
+	{
+		cerr << "Db has: " << walk->key << endl;
+		cerr << "db backward overrun" << endl;
+		exit(1);
+	}
+}
+
+void check(const string& k)
+{
+	check_forward(k);
+	check_backward(k);
+}
+
+void check()
+{
+	check("");
+	check("zzzzzzzzz");
+	check(makeStr()); // ??? change me here for bin
+	check(makeStr()); // ??? change me here for bin
+	check(makeStr()); // ??? change me here for bin
+}
+
+void insert(const string& k)
+{
+	int o = db->insert(k);
+	if (o == -1)
+	{
+		perror("Writer::insert");
+		exit(1);
+	}
+	
+	ram.insert(k);
+}
+
+int main(int argc, char** argv)
+{
+	time_t seed;
+	
+	if (argc == 2)
+		seed = atol(argv[1]);
+	else	seed = time(0);
+	
+	cout << "For replay of this run, seed: " << seed << endl;
+	srandom(seed);
+	
+	db = Writer::open("turnip");
+	if (!db.get())
+	{
+		perror("Writer::open");
+		return 1;
+	}
+	
+	for (int i = 0; i < 1000; ++i)
+	{
+		check();
+		
+		int num = random() % 500; // up to 500 inserts
+		for (int j = 0; j < num; ++j)
+		{
+			string x = makeStr();
+			insert(x);
+			check(x);
+		}
+		
+		check();
+		
+		int c = db->commit();
+		if (c != -1)
+		{
+			perror("Writer::commit");
+			return 1;
+		}
+	}
+	
+	return 0;
+}
