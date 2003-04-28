beebad9cb65cd0aefc1c20d6bd4b5b7ceb00ccf5
terpstra <terpstra@44f0b718-a8ad-45a4-bab0-870672037115>
2003-04-28 19:05:22 +0000

    urls with @ and ! ala lurker
    git-svn-id: https://svn.code.sf.net/p/lurker/code/trunk@1005 44f0b718-a8ad-45a4-bab0-870672037115

--- reg2c/host.c
+++ reg2c/host.c
@@ -1,4 +1,4 @@
-/*  $Id: host.c,v 1.8 2002-10-11 15:48:36 terpstra Exp $
+/*  $Id: host.c,v 1.9 2003-04-28 19:05:22 terpstra Exp $
  *  
  *  host.c - collection of useful web regexps for reg2c
  *  
@@ -29,7 +29,7 @@
 #define INT_REG		"([0-9]+)"
 #define HOST_REG	"((" TOKEN_REG "\\.)*" TOKEN_REG ")"
 #define USER_REG	"([A-Za-z0-9][a-zA-Z0-9._=\\-]*[a-zA-Z0-9]|[a-zA-Z0-9])"
-#define FILE_REG        "([A-Za-z%~0-9:.,_\\-]*[A-Za-z0-9]|#|\\.\\.)"
+#define FILE_REG        "([A-Za-z@!%~0-9:.,_\\-]*[A-Za-z0-9]|#|\\.\\.)"
 #define PATH_REG	"((/" FILE_REG ")+)"
 #define POST_REG   	"([A-Za-z%~0-9.,_=/:;+\\&\\-]+)"
 #define PROTO_REG   	"(([a-zA-Z]-?)*[a-zA-Z])"
