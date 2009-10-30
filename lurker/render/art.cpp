dec2026d917ff4a94272bffbd7d2d389f6a77bb2
terpstra <terpstra@44f0b718-a8ad-45a4-bab0-870672037115>
2009-10-30 18:16:58 +0000

    Forbid art to count quotes as paragraph boundaries
    git-svn-id: https://svn.code.sf.net/p/lurker/code/trunk@1671 44f0b718-a8ad-45a4-bab0-870672037115

--- lurker/render/art.cpp
+++ lurker/render/art.cpp
@@ -15,16 +15,20 @@ find_art_end_l1:
 find_art_end_l2:
 	++c;
 	if (c == end) return out;
-	if (*c < 11) 
+	if (*c < 32) 
 		if (*c < 10) 
 			if (*c < 9) goto find_art_end_l3;
-			else goto find_art_end_l5;
-		else goto find_art_end_l1;
+			else goto find_art_end_l6;
+		else 
+			if (*c < 11) goto find_art_end_l1;
+			else goto find_art_end_l3;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_end_l3;
-			else goto find_art_end_l9;
-		else goto find_art_end_l3;
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l16;
+			else goto find_art_end_l3;
+		else 
+			if (*c < 63) goto find_art_end_l2;
+			else goto find_art_end_l3;
 find_art_end_l3:
 	++c;
 	if (c == end) return out;
@@ -36,7 +40,7 @@ find_art_end_l3:
 	else 
 		if (*c < 33) 
 			if (*c < 32) goto find_art_end_l3;
-			else goto find_art_end_l6;
+			else goto find_art_end_l13;
 		else goto find_art_end_l3;
 find_art_end_l4:
 	++c;
@@ -49,45 +53,160 @@ find_art_end_l4:
 find_art_end_l5:
 	++c;
 	if (c == end) return out;
-	if (*c < 11) 
+	if (*c < 32) 
 		if (*c < 10) 
 			if (*c < 9) goto find_art_end_l4;
-			else goto find_art_end_l5;
-		else goto find_art_end_l1;
+			else goto find_art_end_l6;
+		else 
+			if (*c < 11) goto find_art_end_l1;
+			else goto find_art_end_l4;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_end_l4;
-			else goto find_art_end_l5;
-		else goto find_art_end_l4;
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l10;
+			else goto find_art_end_l4;
+		else 
+			if (*c < 63) goto find_art_end_l5;
+			else goto find_art_end_l4;
 find_art_end_l6:
 	++c;
 	if (c == end) return out;
-	if (*c < 11) 
+	if (*c < 32) 
 		if (*c < 10) 
-			if (*c < 9) goto find_art_end_l3;
+			if (*c < 9) goto find_art_end_l4;
+			else goto find_art_end_l6;
+		else 
+			if (*c < 11) goto find_art_end_l1;
 			else goto find_art_end_l4;
-		else goto find_art_end_l2;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_end_l3;
-			else goto find_art_end_l7;
-		else goto find_art_end_l3;
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l6;
+			else goto find_art_end_l4;
+		else 
+			if (*c < 63) goto find_art_end_l7;
+			else goto find_art_end_l4;
 find_art_end_l7:
 	++c;
+	out = c;
 	if (c == end) return out;
-	if (*c < 11) 
+	if (*c < 32) 
 		if (*c < 10) 
-			if (*c < 9) goto find_art_end_l3;
+			if (*c < 9) goto find_art_end_l4;
+			else goto find_art_end_l7;
+		else 
+			if (*c < 11) goto find_art_end_l8;
 			else goto find_art_end_l4;
-		else goto find_art_end_l2;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_end_l3;
-			else goto find_art_end_l8;
-		else goto find_art_end_l3;
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l7;
+			else goto find_art_end_l4;
+		else 
+			if (*c < 63) goto find_art_end_l7;
+			else goto find_art_end_l4;
 find_art_end_l8:
 	++c;
 	if (c == end) return out;
+	if (*c < 32) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_end_l9;
+			else goto find_art_end_l8;
+		else 
+			if (*c < 11) goto find_art_end_l1;
+			else goto find_art_end_l9;
+	else 
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l8;
+			else goto find_art_end_l9;
+		else 
+			if (*c < 63) goto find_art_end_l8;
+			else goto find_art_end_l9;
+find_art_end_l9:
+	++c;
+	out = c;
+	if (c == end) return out;
+	if (*c < 11) 
+		if (*c < 10) goto find_art_end_l9;
+		else goto find_art_end_l8;
+	else goto find_art_end_l9;
+find_art_end_l10:
+	++c;
+	if (c == end) return out;
+	if (*c < 32) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_end_l4;
+			else goto find_art_end_l6;
+		else 
+			if (*c < 11) goto find_art_end_l1;
+			else goto find_art_end_l4;
+	else 
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l11;
+			else goto find_art_end_l4;
+		else 
+			if (*c < 63) goto find_art_end_l5;
+			else goto find_art_end_l4;
+find_art_end_l11:
+	++c;
+	if (c == end) return out;
+	if (*c < 32) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_end_l4;
+			else goto find_art_end_l6;
+		else 
+			if (*c < 11) goto find_art_end_l1;
+			else goto find_art_end_l4;
+	else 
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l12;
+			else goto find_art_end_l4;
+		else 
+			if (*c < 63) goto find_art_end_l5;
+			else goto find_art_end_l4;
+find_art_end_l12:
+	++c;
+	if (c == end) return out;
+	if (*c < 32) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_end_l4;
+			else goto find_art_end_l6;
+		else 
+			if (*c < 11) goto find_art_end_l1;
+			else goto find_art_end_l4;
+	else 
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l6;
+			else goto find_art_end_l4;
+		else 
+			if (*c < 63) goto find_art_end_l5;
+			else goto find_art_end_l4;
+find_art_end_l13:
+	++c;
+	if (c == end) return out;
+	if (*c < 11) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_end_l3;
+			else goto find_art_end_l4;
+		else goto find_art_end_l2;
+	else 
+		if (*c < 33) 
+			if (*c < 32) goto find_art_end_l3;
+			else goto find_art_end_l14;
+		else goto find_art_end_l3;
+find_art_end_l14:
+	++c;
+	if (c == end) return out;
+	if (*c < 11) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_end_l3;
+			else goto find_art_end_l4;
+		else goto find_art_end_l2;
+	else 
+		if (*c < 33) 
+			if (*c < 32) goto find_art_end_l3;
+			else goto find_art_end_l15;
+		else goto find_art_end_l3;
+find_art_end_l15:
+	++c;
+	if (c == end) return out;
 	if (*c < 11) 
 		if (*c < 10) 
 			if (*c < 9) goto find_art_end_l3;
@@ -98,45 +217,57 @@ find_art_end_l8:
 			if (*c < 32) goto find_art_end_l3;
 			else goto find_art_end_l4;
 		else goto find_art_end_l3;
-find_art_end_l9:
+find_art_end_l16:
 	++c;
 	if (c == end) return out;
-	if (*c < 11) 
+	if (*c < 32) 
 		if (*c < 10) 
 			if (*c < 9) goto find_art_end_l3;
-			else goto find_art_end_l5;
-		else goto find_art_end_l1;
+			else goto find_art_end_l6;
+		else 
+			if (*c < 11) goto find_art_end_l1;
+			else goto find_art_end_l3;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_end_l3;
-			else goto find_art_end_l10;
-		else goto find_art_end_l3;
-find_art_end_l10:
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l17;
+			else goto find_art_end_l3;
+		else 
+			if (*c < 63) goto find_art_end_l2;
+			else goto find_art_end_l3;
+find_art_end_l17:
 	++c;
 	if (c == end) return out;
-	if (*c < 11) 
+	if (*c < 32) 
 		if (*c < 10) 
 			if (*c < 9) goto find_art_end_l3;
-			else goto find_art_end_l5;
-		else goto find_art_end_l1;
+			else goto find_art_end_l6;
+		else 
+			if (*c < 11) goto find_art_end_l1;
+			else goto find_art_end_l3;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_end_l3;
-			else goto find_art_end_l11;
-		else goto find_art_end_l3;
-find_art_end_l11:
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l18;
+			else goto find_art_end_l3;
+		else 
+			if (*c < 63) goto find_art_end_l2;
+			else goto find_art_end_l3;
+find_art_end_l18:
 	++c;
 	if (c == end) return out;
-	if (*c < 11) 
+	if (*c < 32) 
 		if (*c < 10) 
 			if (*c < 9) goto find_art_end_l3;
-			else goto find_art_end_l5;
-		else goto find_art_end_l1;
+			else goto find_art_end_l6;
+		else 
+			if (*c < 11) goto find_art_end_l1;
+			else goto find_art_end_l3;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_end_l3;
-			else goto find_art_end_l5;
-		else goto find_art_end_l3;
+		if (*c < 62) 
+			if (*c < 33) goto find_art_end_l6;
+			else goto find_art_end_l3;
+		else 
+			if (*c < 63) goto find_art_end_l2;
+			else goto find_art_end_l3;
 }
 char* find_art_starts(const char* start, const char* end, char* scratch) {
 	const char* c = end;
@@ -154,7 +285,7 @@ find_art_starts_start:
 	else 
 		if (*c < 33) 
 			if (*c < 32) goto find_art_starts_l1;
-			else goto find_art_starts_l8;
+			else goto find_art_starts_l12;
 		else goto find_art_starts_l1;
 find_art_starts_l1:
 	*--scratch = 0;
@@ -167,7 +298,7 @@ find_art_starts_l1:
 	else 
 		if (*c < 33) 
 			if (*c < 32) goto find_art_starts_l1;
-			else goto find_art_starts_l5;
+			else goto find_art_starts_l9;
 		else goto find_art_starts_l1;
 find_art_starts_l2:
 	*--scratch = 0;
@@ -179,88 +310,100 @@ find_art_starts_l2:
 find_art_starts_l3:
 	*--scratch = 1;
 	if (c-- == start) return scratch;
-	if (*c < 11) 
+	if (*c < 32) 
 		if (*c < 10) 
 			if (*c < 9) goto find_art_starts_l2;
 			else goto find_art_starts_l4;
-		else goto find_art_starts_l0;
+		else 
+			if (*c < 11) goto find_art_starts_l0;
+			else goto find_art_starts_l2;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_starts_l2;
-			else goto find_art_starts_l4;
-		else goto find_art_starts_l2;
+		if (*c < 62) 
+			if (*c < 33) goto find_art_starts_l4;
+			else goto find_art_starts_l2;
+		else 
+			if (*c < 63) goto find_art_starts_l5;
+			else goto find_art_starts_l2;
 find_art_starts_l4:
 	*--scratch = 0;
 	if (c-- == start) return scratch;
-	if (*c < 11) 
+	if (*c < 32) 
 		if (*c < 10) 
 			if (*c < 9) goto find_art_starts_l2;
 			else goto find_art_starts_l4;
-		else goto find_art_starts_l0;
+		else 
+			if (*c < 11) goto find_art_starts_l0;
+			else goto find_art_starts_l2;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_starts_l2;
-			else goto find_art_starts_l4;
-		else goto find_art_starts_l2;
+		if (*c < 62) 
+			if (*c < 33) goto find_art_starts_l4;
+			else goto find_art_starts_l2;
+		else 
+			if (*c < 63) goto find_art_starts_l5;
+			else goto find_art_starts_l2;
 find_art_starts_l5:
 	*--scratch = 0;
 	if (c-- == start) return scratch;
-	if (*c < 11) 
-		if (*c < 10) 
-			if (*c < 9) goto find_art_starts_l1;
-			else goto find_art_starts_l2;
-		else goto find_art_starts_l0;
-	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_starts_l1;
+	if (*c < 33) 
+		if (*c < 11) 
+			if (*c < 10) goto find_art_starts_l2;
+			else goto find_art_starts_l0;
+		else 
+			if (*c < 32) goto find_art_starts_l2;
 			else goto find_art_starts_l6;
-		else goto find_art_starts_l1;
+	else 
+		if (*c < 63) 
+			if (*c < 62) goto find_art_starts_l2;
+			else goto find_art_starts_l5;
+		else goto find_art_starts_l2;
 find_art_starts_l6:
 	*--scratch = 0;
 	if (c-- == start) return scratch;
-	if (*c < 11) 
-		if (*c < 10) 
-			if (*c < 9) goto find_art_starts_l1;
-			else goto find_art_starts_l2;
-		else goto find_art_starts_l0;
-	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_starts_l1;
+	if (*c < 33) 
+		if (*c < 11) 
+			if (*c < 10) goto find_art_starts_l2;
+			else goto find_art_starts_l0;
+		else 
+			if (*c < 32) goto find_art_starts_l2;
 			else goto find_art_starts_l7;
-		else goto find_art_starts_l1;
+	else 
+		if (*c < 63) 
+			if (*c < 62) goto find_art_starts_l2;
+			else goto find_art_starts_l5;
+		else goto find_art_starts_l2;
 find_art_starts_l7:
 	*--scratch = 0;
 	if (c-- == start) return scratch;
-	if (*c < 11) 
-		if (*c < 10) 
-			if (*c < 9) goto find_art_starts_l1;
-			else goto find_art_starts_l2;
-		else goto find_art_starts_l0;
+	if (*c < 33) 
+		if (*c < 11) 
+			if (*c < 10) goto find_art_starts_l2;
+			else goto find_art_starts_l0;
+		else 
+			if (*c < 32) goto find_art_starts_l2;
+			else goto find_art_starts_l8;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_starts_l1;
-			else goto find_art_starts_l2;
-		else goto find_art_starts_l1;
+		if (*c < 63) 
+			if (*c < 62) goto find_art_starts_l2;
+			else goto find_art_starts_l5;
+		else goto find_art_starts_l2;
 find_art_starts_l8:
 	*--scratch = 0;
 	if (c-- == start) return scratch;
-	if (*c < 11) 
-		if (*c < 10) 
-			if (*c < 9) goto find_art_starts_l1;
-			else goto find_art_starts_l4;
-		else goto find_art_starts_l0;
+	if (*c < 62) 
+		if (*c < 11) 
+			if (*c < 10) goto find_art_starts_l2;
+			else goto find_art_starts_l0;
+		else goto find_art_starts_l2;
 	else 
-		if (*c < 33) 
-			if (*c < 32) goto find_art_starts_l1;
-			else goto find_art_starts_l9;
-		else goto find_art_starts_l1;
+		if (*c < 63) goto find_art_starts_l5;
+		else goto find_art_starts_l2;
 find_art_starts_l9:
 	*--scratch = 0;
 	if (c-- == start) return scratch;
 	if (*c < 11) 
 		if (*c < 10) 
 			if (*c < 9) goto find_art_starts_l1;
-			else goto find_art_starts_l4;
+			else goto find_art_starts_l2;
 		else goto find_art_starts_l0;
 	else 
 		if (*c < 33) 
@@ -268,6 +411,58 @@ find_art_starts_l9:
 			else goto find_art_starts_l10;
 		else goto find_art_starts_l1;
 find_art_starts_l10:
+	*--scratch = 0;
+	if (c-- == start) return scratch;
+	if (*c < 11) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_starts_l1;
+			else goto find_art_starts_l2;
+		else goto find_art_starts_l0;
+	else 
+		if (*c < 33) 
+			if (*c < 32) goto find_art_starts_l1;
+			else goto find_art_starts_l11;
+		else goto find_art_starts_l1;
+find_art_starts_l11:
+	*--scratch = 0;
+	if (c-- == start) return scratch;
+	if (*c < 11) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_starts_l1;
+			else goto find_art_starts_l2;
+		else goto find_art_starts_l0;
+	else 
+		if (*c < 33) 
+			if (*c < 32) goto find_art_starts_l1;
+			else goto find_art_starts_l2;
+		else goto find_art_starts_l1;
+find_art_starts_l12:
+	*--scratch = 0;
+	if (c-- == start) return scratch;
+	if (*c < 11) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_starts_l1;
+			else goto find_art_starts_l4;
+		else goto find_art_starts_l0;
+	else 
+		if (*c < 33) 
+			if (*c < 32) goto find_art_starts_l1;
+			else goto find_art_starts_l13;
+		else goto find_art_starts_l1;
+find_art_starts_l13:
+	*--scratch = 0;
+	if (c-- == start) return scratch;
+	if (*c < 11) 
+		if (*c < 10) 
+			if (*c < 9) goto find_art_starts_l1;
+			else goto find_art_starts_l4;
+		else goto find_art_starts_l0;
+	else 
+		if (*c < 33) 
+			if (*c < 32) goto find_art_starts_l1;
+			else goto find_art_starts_l14;
+		else goto find_art_starts_l1;
+find_art_starts_l14:
 	*--scratch = 0;
 	if (c-- == start) return scratch;
 	if (*c < 11) 
